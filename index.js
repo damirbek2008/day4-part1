306ead9f14a756a64ad5dc346dde88c11beef857
damirbek2008 <damirbekkamoliddinov2008@gmail.com>
2024-08-20 13:22:47 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+// -----------------------Example 1 <h1>---------------
+
+
+
+
+// STEP1 CREATE THE ELEMENT
+// const newH1 =document.createElement("h1")
+
+// STEP 2 AND ATTRIBUTES/PROPERTIES
+// newH1.textContent="I like pizza"
+// newH1.id="myH1"
+// newH1.style.color="tomato"
+// newH1.style.textAlign="center"
+// // STEP3  APPENT ELEMENT TO DOM
+
+
+// document.body.append(newH1)
+// document.body.prepend(newH1)
+// document.getElementById("box1").append(newH1)
+// document.getElementById("box1").prepend(newH1)
+
+
+
+
+// const box2=document.getElementById("box2")
+// document.body.insertBefore(newH1 ,box2)
+
+
+// const element= document.querySelectorAll(".box")
+
+// document.body.insertBefore(newH1, element[4])
+
+
+
+
+
+
+// REMOVE THE ELEMENT
+
+
+// document.getElementById("box1").removeChild(newH1)
+// document.body.removeChild()
